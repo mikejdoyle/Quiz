6597cd9b3dad1283dae5dbba05c8515eef25b090
Mike D <mike@buzzvertical.com>
2013-11-25 19:38:57 -0600

    first rough html

---
+++ js/quiz.js
@@ -0,0 +1,14 @@
+//file:///Users/michaeldoyle/coding_projects/quiz/index.html
+function slctrl ($scope){
+  $scope.itemlist = [];
+  $scope.additem = function(){if($.trim($scope.itemtext)!="")$scope.itemlist.push({text:$scope.itemtext,bought:false});$scope.itemtext = "";}
+  $scope.strike = function(item){item.bought?(item.bought=false):(item.bought=true);}
+  $scope.clear = function(){
+          var oldList = $scope.itemlist;
+          $scope.itemlist = [];
+          angular.forEach (oldList, function (item){
+            if(!item.bought) $scope.itemlist.push(item);
+          });
+  };
+}//end of crtl
+
