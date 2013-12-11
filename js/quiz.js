f55c8f83891d79d35076ad043c52fd009b70aa4d
Mike D <mike@buzzvertical.com>
2013-12-11 16:35:44 -0600

    minor image choice revisions

--- js/quiz.js
+++ js/quiz.js
@@ -7,7 +7,7 @@ var qs =[
     question: "Thank you for the cookies. I look forward to tossing them",
     choices:  [" Twins", " The Running Man", " Batman and Robin", " Predator"],
     correct:  "Twins",
-    image: "http://4.bp.blogspot.com/-yIRgWjIhW54/UiLO3k-wK4I/AAAAAAAAPWU/AkfaWcFA0bY/s1600/2013-07-19+224.png"
+    image: "http://assets.nydailynews.com/polopoly_fs/1.1113853!/img/httpImage/image.jpg_gen/derivatives/landscape_635/m8dtwin-ec001-h-web.jpg"
   },
 
   {
@@ -38,6 +38,27 @@ var qs =[
     image: "http://www.mostlybymotorcycle.com/wp-content/uploads/2012/02/Arnie.jpg"
   }];
 
+function preloadImages(array) {
+   if (!preloadImages.list) {
+       preloadImages.list = [];
+   }
+   for (var i = 0; i < array.length; i++) {
+       var img = new Image();
+       img.src = array[i];
+       preloadImages.list.push(img);
+   }
+}
+
+var imageURLs = [
+   "http://assets.nydailynews.com/polopoly_fs/1.1113853!/img/httpImage/image.jpg_gen/derivatives/landscape_635/m8dtwin-ec001-h-web.jpg",
+   "https://static.squarespace.com/static/51b3dc8ee4b051b96ceb10de/51ce6099e4b0d911b4489b79/51ce61efe4b0d911b44a69de/1272517437147/1000w/commando5.jpg",
+   "http://www.empireonline.com/images/uploaded/arnold-schwarzenegger-last-action-hero.jpg",
+   "http://cdn.wegotthiscovered.com/wp-content/uploads/Total-Recall-Blu-Review-Pic-2-670x360.jpg",
+   "http://www.mostlybymotorcycle.com/wp-content/uploads/2012/02/Arnie.jpg"
+];
+
+preloadImages(imageURLs);
+
 app.controller("quizCtrl", function ($scope){
   $scope.showCorrect = "";
   $scope.done = false;
@@ -57,7 +78,9 @@ app.controller("quizCtrl", function ($scope){
          $scope.user.wrong++;
        }
       $scope.showCorrect=$scope.questions[$scope.index].correct;
-      $('.backimage').fadeOut().css('background-image','url("'+correctUrl+'")').fadeIn();
+      $('.backimage').fadeOut('slow', function(){
+        $(this).css('background-image','url("'+correctUrl+'")').fadeIn();
+      });
       if(this.index < $scope.qCount-1){
        $scope.index++;
       }
