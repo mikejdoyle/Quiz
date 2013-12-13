d3c096df359b7d5ff91fe0c207331d518c06150f
Mike D <mike@buzzvertical.com>
2013-12-12 22:09:55 -0600

    fixed score bug and final score answer issue

--- js/quiz.js
+++ js/quiz.js
@@ -5,35 +5,35 @@ var app= angular.module ("quiz", ["ngAnimate"]);
 var qs =[
   {
     question: "Thank you for the cookies. I look forward to tossing them",
-    choices:  [" Twins", " The Running Man", " Batman and Robin", " Predator"],
+    choices:  ["Twins", "The Running Man", "Batman and Robin", "Predator"],
     correct:  "Twins",
     image: "http://assets.nydailynews.com/polopoly_fs/1.1113853!/img/httpImage/image.jpg_gen/derivatives/landscape_635/m8dtwin-ec001-h-web.jpg"
   },
 
   {
     question: "Don't disturb my friend, he's dead tired.",
-    choices:  [" Last Action Hero", " Commando", " Conan the Barbarian", " Predator"],
+    choices:  ["Last Action Hero", "Commando", "Conan the Barbarian", "Predator"],
     correct:  "Commando",
     image: "https://static.squarespace.com/static/51b3dc8ee4b051b96ceb10de/51ce6099e4b0d911b4489b79/51ce61efe4b0d911b44a69de/1272517437147/1000w/commando5.jpg"
   },
 
   {
     question: "I'm the famous comedian Arnold Braunschweiger.",
-    choices:  [" Twins", " Junior", " Last Action Hero", " Raw Deal"],
+    choices:  ["Twins", "Junior", "Last Action Hero", "Raw Deal"],
     correct:  "Last Action Hero",
     image: "http://www.empireonline.com/images/uploaded/arnold-schwarzenegger-last-action-hero.jpg"
   },
 
   {
     question: "If I am not me, then who the hell am I?",
-    choices:  [" Total Recall", " Terminator 3: Rise of the Machines", " Commando", " The Running Man"],
+    choices:  ["Total Recall", "Terminator 3: Rise of the Machines", "Commando", "The Running Man"],
     correct:  "Total Recall",
     image: "http://cdn.wegotthiscovered.com/wp-content/uploads/Total-Recall-Blu-Review-Pic-2-670x360.jpg"
   },
 
   {
     question: "Hasta la vista, baby!",
-    choices:  [" The Terminator", " Terminator 2 Judgement Day", " Kindergarden Cop", " Commando"],
+    choices:  ["The Terminator", "Terminator 2 Judgement Day", "Kindergarden Cop", "Commando"],
     correct:  "Terminator 2 Judgement Day",
     image: "http://www.mostlybymotorcycle.com/wp-content/uploads/2012/02/Arnie.jpg"
   }];
